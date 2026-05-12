6878564d9a946c1d2afe0e699b0898f291ae1746
wampixel <wampixel@gmail.com>
2026-05-12 02:04:37 +0200

    [FIX] lint

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub(crate) mod errors;
 pub(crate) mod evaluation_engine;
 
 pub use alert::{Alert, AlertSink, StderrJsonSink};
-pub use configuration::{load as load_configuration, Configuration};
+pub use configuration::{Configuration, load as load_configuration};
 pub use datasources::{DatasourceType, QuickwitSource, StdinSource};
 pub use errors::Error;
 pub use evaluation_engine::{Evaluate, EvaluationContext, PreparedRule};
