f3ab1534135ef4b1c8e7d5fa93f792a56f13ead9
wampixel <wampixel@gmail.com>
2026-05-13 17:15:15 +0200

    [FIX] intialization function

--- src/datasources.rs
+++ src/datasources.rs
@@ -5,12 +5,27 @@ pub use quickwit::QuickwitSource;
 pub use stdin::StdinSource;
 
 use serde::Deserialize;
+use serde_json::Value;
+
+use crate::errors::DatasourceError;
 
 #[derive(Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum DatasourceType {
     Stdin,
     Quickwit { url: String, index: String },
+}
+
+pub async fn run<F: FnMut(Value)>(
+    datasource: DatasourceType,
+    on_event: F,
+) -> Result<(), DatasourceError> {
+    match datasource {
+        DatasourceType::Stdin => StdinSource::new().stream(on_event).await,
+        DatasourceType::Quickwit { url, index } => {
+            QuickwitSource::new(url, index).stream(on_event).await
+        }
+    }
 }
 
 #[cfg(test)]
--- src/evaluation_engine.rs
+++ src/evaluation_engine.rs
@@ -1,13 +1,20 @@
-use serde_json::Value;
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::HashMap, path::Path, sync::Arc};
 
 use kompiler::Filters;
+use serde_json::Value;
 
 mod condition;
 mod filter;
 mod rule;
 
 pub use rule::PreparedRule;
+
+pub fn load_rules(path: &Path) -> Result<Vec<PreparedRule>, crate::Error> {
+    Ok(kompiler::parse_rules(path)?
+        .into_iter()
+        .map(PreparedRule::from)
+        .collect())
+}
 
 pub struct EvaluationContext {
     pub filters: Arc<HashMap<String, Filters>>,
