b64de7311a0c47bbb8515aa10ac6d9f69da84325
wampixel <wampixel@gmail.com>
2026-05-13 17:12:55 +0200

    [FIX] onefile store

--- src/store.rs
+++ src/store.rs
@@ -1,1 +1,33 @@
-pub(crate) mod alert;
+use konnect::{PgPool, Store};
+
+use crate::Error;
+
+pub struct AlertStore {
+    pool: PgPool,
+}
+
+impl Store for AlertStore {
+    fn new(pool: PgPool) -> Self {
+        Self { pool }
+    }
+
+    fn pool(&self) -> &PgPool {
+        &self.pool
+    }
+}
+
+impl AlertStore {
+    pub async fn setup(config: &konnect::DatabaseConfig) -> Result<Self, Error> {
+        let pool = konnect::init(config).await.map_err(Error::DatabaseError)?;
+        let store = Self::new(pool);
+        store.migrate().await?;
+        Ok(store)
+    }
+
+    pub async fn migrate(&self) -> Result<(), Error> {
+        sqlx::migrate!("./migrations")
+            .run(self.pool())
+            .await
+            .map_err(Error::MigrationError)
+    }
+}
